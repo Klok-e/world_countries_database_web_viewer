998d8d3a50d5bef7d79beb91e890dbf5486d58a4
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2019-12-24 21:50:12 +0200

    stuff

--- js/jsgrid_helper.js
+++ js/jsgrid_helper.js
@@ -1,3 +1,55 @@
+var ForeignKeyField = function (config) {
+    jsGrid.Field.call(this, config);
+};
+
+ForeignKeyField.prototype = new jsGrid.Field({
+    itemTemplate: function (value) {
+        //return new Date(value).toDateString();
+        return value;
+    },
+
+    insertTemplate: function (value) {
+        $("<input>").jsGrid({
+            width: 200,
+            height: 200,
+
+            data: [1, 2, 3, 4]
+        });
+        console.log("insertTemplate");
+        //return this._insertPicker = $("<input>").datepicker({ defaultDate: new Date() });
+        return this._insert_val = value
+    },
+
+    editTemplate: function (value) {
+        let picker_grid = $("<div>");
+        document.body.appendChild(picker_grid.get(0));
+
+        picker_grid.jsGrid({
+            width: 200,
+            height: 200,
+
+            data: [1, 2, 3, 4]
+        });
+        console.log("editTemplate");
+        let inp = $("<input>");
+        inp.value = value;
+        return inp
+    },
+
+    insertValue: function () {
+        console.log("insertValue");
+        //return this._insertPicker.datepicker("getDate").toISOString();
+        return this._insert_val;
+    },
+
+    editValue: function () {
+        console.log("editValue");
+        //return this._editPicker.datepicker("getDate").toISOString();
+        return this._edit_val;
+    }
+});
+jsGrid.fields.foreignKeyField = ForeignKeyField;
+
 function create_grid(gridId, page_name, fields) {
     let previousItem;
     $(gridId).jsGrid({
@@ -62,7 +114,13 @@ function create_grid(gridId, page_name, fields) {
                         d.reject()
                     },
                     success: function (data, status, jqXHR) {
-                        d.resolve(data)
+                        if (jqXHR.status === 278) {
+                            $(gridId).jsGrid("clearInsert");
+                            show_warning("Database Error", data.error_msg);
+                            d.reject();
+                        } else {
+                            d.resolve(data)
+                        }
                     },
                 });
                 return d.promise()
@@ -91,4 +149,13 @@ function create_grid(gridId, page_name, fields) {
 
         fields: fields
     });
+}
+
+function show_warning(header, message) {
+    let prot = $("#warning_card_prot");
+    let new_card = prot.clone();
+    new_card.removeAttr("style");
+    new_card.insertAfter(prot);
+    new_card.find(".card-title").text(header);
+    new_card.find(".card-body").text(message);
 }
--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,12 @@
+use r2d2_oracle::oracle;
+use rocket::{
+    http::{ContentType, Status},
+    response,
+    response::Responder,
+    Request, Response,
+};
+use rocket_contrib::json::Json;
+
 #[derive(Debug)]
 pub enum Error {
     OracleError(r2d2_oracle::oracle::Error),
@@ -9,5 +18,32 @@ pub enum Error {
 impl From<r2d2_oracle::oracle::Error> for Error {
     fn from(error: r2d2_oracle::oracle::Error) -> Self {
         Error::OracleError(error)
+    }
+}
+
+impl<'r> Responder<'r> for Error {
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        match self {
+            Error::OracleError(or_error) => {
+                if let oracle::Error::OciError(_) = or_error {
+                    Response::build_from(
+                        json!({ "error_msg": format!("{}", or_error) }).respond_to(req)?,
+                    )
+                    .status(Status::new(278, "A constraint was violated"))
+                    .header(ContentType::JSON)
+                    .ok()
+                } else {
+                    Err(Status::new(500, "oracle"))
+                }
+            }
+            Error::TableEmptyError { .. } => Err(Status::new(500, "oracle")),
+            Error::KeyDoesntExistError { .. } => Err(Status::new(
+                400,
+                "Entity with the specified key doesn't exist",
+            )),
+            Error::TableDoesntExistError { .. } => {
+                Err(Status::new(400, "Specified table doesn't exist"))
+            }
+        }
     }
 }
